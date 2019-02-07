86d3cb8986079fd56bb5fa5f506abb9e5f134f28
Angelos Chalaris <chalarangelo@gmail.com>
2019-02-07 22:25:16 +0200

    Production build and deployment

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # elegantime
 
+![Splash](splash.png)
+
 Elegant React-based clock
+
+* Videos: [4K Relaxation Channel - Youtube](https://www.youtube.com/channel/UCg72Hd6UZAgPBAUZplnmPMQ/videos)
+* Icons: [Feather - Cole Bemis](https://feathericons.com/)
---
+++ build/precache-manifest.3f77704c1180120111068f30b995c9e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "50ce8aa7c9f2fed6af30",
+    "url": "/static/js/main.50ce8aa7.chunk.js"
+  },
+  {
+    "revision": "dc94ae0206aa2f16d223",
+    "url": "/static/js/1.dc94ae02.chunk.js"
+  },
+  {
+    "revision": "50ce8aa7c9f2fed6af30",
+    "url": "/static/css/main.2b65e86b.chunk.css"
+  },
+  {
+    "revision": "38cabf015eadd84de9b953ca59bee314",
+    "url": "/index.html"
+  }
+];
