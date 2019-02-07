a52198645d8e0076dc9ce35db0cdcda4392ac448
Angelos Chalaris <chalarangelo@gmail.com>
2019-02-07 22:09:44 +0200

    Background video controls

--- src/VideoBackground.js
+++ src/VideoBackground.js
@@ -9,6 +9,14 @@ class VideoBackground extends Component {
       videos: videoArray,
       index: 0
     }
+    this.previous = this.previous.bind(this);
+    this.next = this.next.bind(this);
+  }
+
+  previous() {
+    this.setState(previous => ({
+      index: (previous.index === 0) ? (previous.videos.length - 1) : --previous.index
+    }));
   }
 
   next() {
@@ -35,6 +43,8 @@ class VideoBackground extends Component {
       <div className="Video-Container">
         <div className="Video-Overlay"></div>
         <iframe title="Background video" className="Video-Background" src={`https://www.youtube.com/embed/${this.state.videos[this.state.index].url}?controls=0&amp;start=${this.state.videos[this.state.index].start}&amp;end=${this.state.videos[this.state.index].end}&amp;autoplay=1&amp;mute=1&amp;html5=True&amp;hd=1`} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        <button className="Video-Previous-Button" onClick={this.previous}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="11 17 6 12 11 7"></polyline><polyline points="18 17 13 12 18 7"></polyline></svg></button>
+        <button className="Video-Next-Button" onClick={this.next}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="13 17 18 12 13 7"></polyline><polyline points="6 17 11 12 6 7"></polyline></svg></button>
       </div>
     )
   }
