920bd279107839d11b8dfc496d4ef55230ea217e
Angelos Chalaris <chalarangelo@gmail.com>
2019-02-07 14:23:05 +0200

    Forced HD video

--- src/VideoBackground.js
+++ src/VideoBackground.js
@@ -9,7 +9,7 @@ class VideoBackground extends Component {
     return (
       <div className="Video-Container">
         <div className="Video-Overlay"></div>
-        <iframe className="Video-Background" src="https://www.youtube.com/embed/HSsqzzuGTPo?controls=0&amp;start=39&amp;end=13120&amp;autoplay=1&amp;mute=1&amp;html5=True" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe className="Video-Background" src="https://www.youtube.com/embed/HSsqzzuGTPo?controls=0&amp;start=39&amp;end=13120&amp;autoplay=1&amp;mute=1&amp;html5=True&amp;hd=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
     )
   }
